9797adf7b4a717469c6556d7cb80a3136d45419b
Maxence <zemoax@hotmail.fr>
2021-04-20 16:06:29 +0200

    affichage du prix classique sur les promo

--- wp-content/themes/stop-covi2/woocommerce/content-widget-product.php
+++ wp-content/themes/stop-covi2/woocommerce/content-widget-product.php
@@ -23,10 +23,21 @@ if ( ! is_a( $product, 'WC_Product' ) ) {
 
 	<?php $on_sale = false; if ($product->get_sale_price()) { $on_sale = true; } ?>
 
-	<a class="image <?php if ($on_sale) { ?>on-sale<?php } ?>" href="<?= esc_url( $product->get_permalink() ); ?>">
-		<?= $product->get_image(); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-		<?= $product->get_price_html(); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-	</a>
+	<?php if ($on_sale) : ?>
+		<a class="image on-sale" href="<?= esc_url( $product->get_permalink() ); ?>">
+			<?= $product->get_image(); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<span class="woocommerce-Price-amount amount">
+				<del><i><?= number_format((float)$product->get_regular_price(), 2, ',', '') . "€"; ?></i></del>
+				<span> <?= number_format((float)$product->get_sale_price(), 2, ',', '') . "€"; ?></span>
+			</span>
+		</a>
+	<?php else : ?>
+		<a class="image" href="<?= esc_url( $product->get_permalink() ); ?>">
+			<?= $product->get_image(); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<?= $product->get_price_html(); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+		</a>
+	<?php endif; ?>
+
 
 	<a class="title <?php if ($on_sale) { ?>on-sale<?php } ?>" href="<?= esc_url( $product->get_permalink() ); ?>">
 		<span class="product-title"><?= wp_kses_post( $product->get_name() ); ?></span>
